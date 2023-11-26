2e52b454fdeec7b7be914d10bde7edb5ac094821
khattab88 <m_khatab_88@hotmail.com>
2023-11-26 23:29:43 +0200

    create entity classes

---
+++ FormulaOne.Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int Status { get; set; }
+    }
+}
---
+++ FormulaOne.Entities/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Entities
+{
+    public class Driver : BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int DriverNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public virtual ICollection<Achievement> Achievements { get; set; }
+
+        public Driver()
+        {
+            Achievements = new HashSet<Achievement>();
+        }
+    }
+}

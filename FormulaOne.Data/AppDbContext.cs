e72273462f06228d75733c3cbca5e821f58738ee
khattab88 <m_khatab_88@hotmail.com>
2023-11-27 12:42:09 +0200

    add db context

---
+++ FormulaOne.Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using FormulaOne.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Driver> Drivers { get; set; }
+        public virtual DbSet<Achievement> Achievements { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Achievement>(entity =>
+            {
+                entity.HasOne<Driver>(a => a.Driver)
+                    .WithMany(d => d.Achievements)
+                    .HasForeignKey(a => a.DriverId)
+                    .OnDelete(DeleteBehavior.NoAction)
+                    .HasConstraintName("FK_Achievements_Driver");
+            });
+        }
+    }
+}

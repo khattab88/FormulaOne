92663f9c563d85017f08132a2f644b656589e48d
khattab88 <m_khatab_88@hotmail.com>
2023-11-27 20:07:39 +0200

    create controllers

---
+++ FormulaOne.API/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FormulaOne.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FormulaOne.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly IMapper _mapper;
+
+        public BaseController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
---
+++ FormulaOne.API/Controllers/DriversController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using FormulaOne.Data.Repositories.Interfaces;
+
+namespace FormulaOne.API.Controllers
+{
+    public class DriversController : BaseController
+    {
+        public DriversController(IUnitOfWork unitOfWork, IMapper mapper) 
+            : base(unitOfWork, mapper)
+        {
+        }
+    }
+}
